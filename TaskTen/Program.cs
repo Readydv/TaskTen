1e10d5ecbcd3971f74e2066dc9ca8d534c82fac3
Danil <daniil-vn@mail.ru>
2024-11-19 04:04:26 +0500

    Написана программа "Мини калькулятор", которая считает сумму 2-х чисел.

--- TaskTen/Program.cs
+++ TaskTen/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+using System.IO;
+
+namespace MiniCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISumCalculator sumCalculator = new SumCalculator();
+            double number1 = 0;
+            double number2 = 0;
+
+            try
+            {
+                Console.WriteLine("Введите первое число:");
+                number1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                number2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = sumCalculator.Sum(number1, number2);
+                Console.WriteLine($"Сумма равна: {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введенное значение не является числом");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: Введенное значение слишком велико или слишком мало");
+            }
+            finally
+            {
+                Console.WriteLine("Вычисление завершено");
+            }
+        }
+    }
+
+    public interface ISumCalculator
+    {
+        double Sum(double num1, double num2);
+    }
+
+    public class SumCalculator : ISumCalculator
+    {
+        public double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
